a95ffd030b1ec683ea05d025cea3a68db6a96137
shrutikapoor08 <shruti.kapoor08@gmail.com>
2024-02-04 14:46:00 -0800

    Adding solution for exercise

--- 3-creating-your-first-component/src/App.jsx
+++ 3-creating-your-first-component/src/App.jsx
@@ -6,10 +6,15 @@ const Name = ({ name }) => {
   return <p className="name-heading">Hi, {name}</p>;
 };
 
+const ProfilePicture = ({ imgSrc }) => {
+  return <img className="profile-picture" src={imgSrc} />;
+};
+
 const App = () => {
   return (
     <div className="App">
       <Name name="Shruti Kapoor" />
+      <ProfilePicture imgSrc="https://avatars.githubusercontent.com/u/2525914?v=4" />
     </div>
   );
 };
