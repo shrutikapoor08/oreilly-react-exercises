62b61976983353e6f70189630f600047a66a9f27
shrutikapoor08 <shruti.kapoor08@gmail.com>
2024-04-21 00:41:48 -0700

    React hooks

--- 5-hooks/useContext/src/App.jsx
+++ 5-hooks/useContext/src/App.jsx
@@ -1,71 +1,19 @@
-import React, { useState, createContext, useContext } from "react";
-import "./styles.css";
-
-const ThemeContext = createContext(null);
-
-const Button = ({ children, onClick }) => {
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  const handleClick = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
-  const className = "button-" + theme;
-  return (
-    <button onClick={handleClick} className={className}>
-      {children}
-    </button>
-  );
-};
+import React from "react";
+import "./App.css";
+import Button from "./Button";
+import ThemeContext from "./ThemeContext";
+import useTheme from "./hooks/useTheme";
 
 const App = () => {
-  const [theme, setTheme] = useState("light");
+  const { theme, setTheme } = useTheme();
 
   return (
     <div className={`app theme-${theme}`}>
       <ThemeContext.Provider value={{ theme, setTheme }}>
-        <Button>Use {theme === "dark" ? "light" : "dark"} mode</Button>
+        <Button />
       </ThemeContext.Provider>
     </div>
   );
 };
 
 export default App;
-
-// import React, { useState, createContext, useContext } from "react";
-
-// const ThemeContext = createContext(null);
-
-// const useTheme = () => {
-//   const [theme, setTheme] = useState("light");
-//   return { theme, setTheme };
-// };
-
-// const Button = ({ children }) => {
-//   const { theme, setTheme } = useContext(ThemeContext);
-
-//   const handleClick = () => {
-//     setTheme(theme === "light" ? "dark" : "light");
-//   };
-
-//   const className = "button-" + theme;
-//   return (
-//     <button onClick={handleClick} className={className}>
-//       {children}
-//     </button>
-//   );
-// };
-
-// const App = () => {
-//   const { theme } = useTheme();
-
-//   return (
-//     <div className={`app theme-${theme}`}>
-//       <ThemeContext.Provider value={useTheme()}>
-//         <Button>Use {theme === "dark" ? "light" : "dark"} mode</Button>
-//       </ThemeContext.Provider>
-//     </div>
-//   );
-// };
-
-// export default App;
